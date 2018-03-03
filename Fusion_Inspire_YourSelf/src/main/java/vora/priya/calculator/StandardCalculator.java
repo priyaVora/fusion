108bbaef562b78c83cb0c8588aaa957945ddf9ac
priyaVora <prvora89@gmail.com>
2018-03-02 19:09:16 -0700

    Fixed mistakes regarding classes...

--- Fusion_Inspire_YourSelf/src/main/java/vora/priya/calculator/StandardCalculator.java
+++ Fusion_Inspire_YourSelf/src/main/java/vora/priya/calculator/StandardCalculator.java
@@ -1,5 +1,60 @@
 package vora.priya.calculator;
 
-public class StandardCalculator {
+import java.util.HashMap;
+import java.util.Map;
 
+import vora.priya.enums.CalculatorType;
+
+public class StandardCalculator {
+	CalculatorType type;
+
+	public static void main(String[] args) {
+		StandardCalculator c = new StandardCalculator();
+		Map<Integer, String> hasMap = new HashMap<Integer, String>();
+		
+		System.out.println(c.squareRoot(25.0));
+	}
+
+	public Double add(Double input1, Double input2) {
+		Double value = input1 + input2;
+		return value;
+	}
+
+	public Double subtract(Double input1, Double input2) {
+		Double value = input1 - input2;
+		return value;
+	}
+
+	public Double multiple(Double input1, Double input2) {
+		Double value = input1 * input2;
+		return value;
+	}
+
+	public Double divide(Double input1, Double input2) {
+		Double value = (input1 / input2);
+		return value;
+	}
+
+	public Double squareRoot(Double input) {
+		Double t;
+		Double squareRoot = input / 2;
+
+		do {
+			t = squareRoot;
+			int divideInput = (int) (input / t);
+			double value = t + divideInput;
+			squareRoot = (value / 2);
+		} while ((t - squareRoot) != 0);
+
+		return squareRoot;
+	}
+
+	public Double square(Double input) {
+		Double value = multiple(input, input);
+		return value;
+	}
+
+	public void rationFunction() {
+
+	}
 }
--- Fusion_Inspire_YourSelf/src/main/java/vora/priya/calculator/Standard_Conversions.java
+++ Fusion_Inspire_YourSelf/src/main/java/vora/priya/calculator/Standard_Conversions.java
@@ -1,55 +1,7 @@
 package vora.priya.calculator;
 
-import vora.priya.enums.CalculatorType;
+
 
 public class Standard_Conversions {
-	CalculatorType type;
-
-	public static void main(String[] args) {
-		Standard_Conversions c = new Standard_Conversions();
-		System.out.println(c.squareRoot(25.0));
-	}
-
-	public Double add(Double input1, Double input2) {
-		Double value = input1 + input2;
-		return value;
-	}
-
-	public Double subtract(Double input1, Double input2) {
-		Double value = input1 - input2;
-		return value;
-	}
-
-	public Double multiple(Double input1, Double input2) {
-		Double value = input1 * input2;
-		return value;
-	}
-
-	public Double divide(Double input1, Double input2) {
-		Double value = (input1 / input2);
-		return value;
-	}
-
-	public Double squareRoot(Double input) {
-		Double t;
-		Double squareRoot = input / 2;
-
-		do {
-			t = squareRoot;
-			int divideInput = (int) (input / t);
-			double value = t + divideInput;
-			squareRoot = (value / 2);
-		} while ((t - squareRoot) != 0);
-
-		return squareRoot;
-	}
-
-	public Double square(Double input) {
-		Double value = multiple(input, input);
-		return value;
-	}
-
-	public void rationFunction() {
-
-	}
+	
 }
