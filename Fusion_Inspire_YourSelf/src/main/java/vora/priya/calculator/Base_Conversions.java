01d0b1a2738401b2ccea1a373bb3a112c8f06126
priyaVora <prvora89@gmail.com>
2018-03-02 22:06:10 -0700

    Decimal To binary Conversion Works.

--- Fusion_Inspire_YourSelf/src/main/java/vora/priya/calculator/Base_Conversions.java
+++ Fusion_Inspire_YourSelf/src/main/java/vora/priya/calculator/Base_Conversions.java
@@ -1,8 +1,44 @@
 package vora.priya.calculator;
 
-public class Base_Conversions {
-	public void Decimal_To_Binary() {
+import vora.priya.enums.Programming_Conversions;
 
+public class Base_Conversions {
+	Programming_Conversions conversionType;
+
+	public static void main(String[] args) {
+		Base_Conversions conversion = new Base_Conversions();
+		System.out.println(conversion.Decimal_To_Binary(78.0));
+	}
+
+	public String Decimal_To_Binary(Double value) {
+		conversionType = Programming_Conversions.BINARY;
+		int leftOver = Integer.MAX_VALUE;
+		Integer remainder;
+		Integer newValue = (int) Math.floor(value);
+		String binarySequence = "";
+		String[] sequence = { "0", "0", "0", "0", "0", "0", "0", "0" };
+		int val = newValue;
+		int count = 7;
+		while (leftOver != 0) {
+			leftOver = (int) Math.floor(newValue);
+			remainder = getRemainderBinary(leftOver);
+			if (leftOver == 0) {
+				break;
+			}
+			sequence[count] = "" + remainder;
+			count--;
+			newValue = (int) (val / conversionType.getBaseNumber());
+			val = newValue;
+		}
+		for (String octet : sequence) {
+			binarySequence += octet;
+		}
+		return binarySequence;
+	}
+
+	public Integer getRemainderBinary(int value) {
+		Integer answer = (value % 2);
+		return answer;
 	}
 
 	public void Decimal_To_Hexdecimal() {
---
+++ Fusion_Inspire_YourSelf/src/main/java/vora/priya/enums/Programming_Conversions.java
@@ -0,0 +1,19 @@
+package vora.priya.enums;
+
+public enum Programming_Conversions {
+	BINARY(2.0), DECIMAL(10.0), HEXADECIMAL(6.0), OCTAL(8.0);
+
+	private Double baseNumber;
+
+	private Programming_Conversions(Double baseNumber) {
+		this.baseNumber = baseNumber;
+	}
+
+	public Double getBaseNumber() {
+		return baseNumber;
+	}
+
+	public void setBaseNumber(Double baseNumber) {
+		this.baseNumber = baseNumber;
+	}
+}
